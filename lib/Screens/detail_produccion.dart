ab5e0f1825be675bcda6e4e0381e84d497dd59dd
cristianfchq <cristian.fchq@gmail.com>
2019-10-23 00:14:34 -0400

    login

---
+++ lib/Screens/detail_produccion.dart
@@ -0,0 +1,65 @@
+//import 'package:AeologicSplashDemo/Constant/Constant.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:login/Constant/Constant.dart';
+
+class detailProduccion extends StatelessWidget {
+  detailProduccion(this.userName);
+
+  String userName;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
+        backgroundColor: Color(0xFF54C5F8),
+        title: new Text("PRODUCCION"),
+      ),
+      body: new Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 40.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 0.0),
+                child: new Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    new Text("Welcome,",
+                        style: TextStyle(
+                            fontWeight: FontWeight.normal, fontSize: 20.0)),
+                    new Row(
+                      children: <Widget>[],
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: new Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    new Text(userName,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18.0))
+                  ],
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+    // TODO: implement build
+  }
+}
